e1ee6488722e30d0b5de1a9ea2432a4335d3c773
Meltests <amel.dahmani.etu@outlook.fr>
2025-05-13 16:41:56 +0200

    Add files via upload

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,3 +1,4 @@
+//tableau images de mon slider//
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -17,10 +18,52 @@ const slides = [
 	}
 ]
 
+//Evenement click sur fléche slider//
 document.querySelector('.arrow_left').addEventListener('click', () => {
-	console.log("Flèche gauche cliquée");
-  });
+  if (currentIndex > 0) {
+    currentIndex--; // -- pour l'ordre antichronologique//
+  } else {
+    currentIndex = slides.length - 1; //slide lenght pour calculer nombre d'element dans l'index// 
+  }
+  updateSlide(currentIndex);
+  console.log("Flèche gauche cliquée");
+});
 
-  document.querySelector('.arrow_right').addEventListener('click', () => {
-	console.log("Flèche droite cliquée");
-  });
+document.querySelector('.arrow_right').addEventListener('click', () => {
+  if (currentIndex < slides.length - 1) {
+    currentIndex++; //ici ++ pour avancer dans le tableau index en ordre chronologique// 
+  } else {
+    currentIndex = 0;
+  }
+  updateSlide(currentIndex);
+  console.log("Flèche droite cliquée");
+});
+
+
+//Bullet point//
+  const dotsContainer = document.querySelector('.dots'); //appeler la classe de mes dots//
+
+slides.forEach((_, index) => { //j'appele les slides de mon tableau pour y attribuer un dot à chacune// 
+	const dot = document.createElement('span'); //je creer sur mon html des span pour chaque dots//
+  dot.classList.add('dot'); //propriétées appliquées à mes dots"
+  dotsContainer.appendChild(dot); //je creer l'element enfant dans l'element parent dotscontainer//
+  if (index === 0) {
+  dot.classList.add('dot_selected'); // à modifier car non fonctionnel : provisoir//
+} //le dot de la page sera remplis car c'est celui selectionné// 
+});
+
+
+//banner//
+
+let currentIndex = 0;
+
+const bannerImg = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p"); 
+
+
+function updateSlide(index) { //elements qui vont changer au changement de slide//
+  const slide = slides[index];
+  bannerImg.src = `./assets/images/slideshow/${slide.image}`; //chemin de la nouvelle image"
+  bannerImg.alt = `Image slide ${index + 1}`; //texte associé à nouvelle image//
+  bannerText.innerHTML = slide.tagLine; //tagline de la slide//
+}
