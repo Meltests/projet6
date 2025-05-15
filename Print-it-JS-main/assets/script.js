9061820491dbe31257c5aa1cd7db4f7944264719
Meltests <amel.dahmani.etu@outlook.fr>
2025-05-15 21:52:05 +0200

    Add files via upload

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -19,44 +19,52 @@ const slides = [
 
 //Bullet point//
   const dotsContainer = document.querySelector('.dots'); //appeler la classe de mes dots//
+  
   function createDot() {
 slides.forEach((_, index) => { //j'appele les slides de mon tableau pour y attribuer un dot à chacune// 
 	const dot = document.createElement('span'); //je creer sur mon html des span pour chaque dots//
   dot.classList.add('dot'); //propriétées appliquées à mes dots"
   dotsContainer.appendChild(dot); //je creer l'element enfant dans l'element parent dotscontainer// 
 });
+ document.querySelectorAll('.dots .dot')[0].classList.add('dot_selected');
 }
-
 createDot();
 
 
 
 //Evenement click sur fléche slider//
-document.querySelector('.arrow_left').addEventListener('click', () => { 
-  const currentdot = document.querySelectorAll('.dots .dot');
-    currentdot[currentIndex].classList.remove('dot_selected');
+function arrowleftClick() { // je creer une fonction pour pouvoir la réutiliser plus tard dans mon code//
+  const currentdot = document.querySelectorAll('.dots .dot'); //j'apelle les class de mon bullet point//
+  currentdot[currentIndex].classList.remove('dot_selected'); // je leur enleve le remplissage du bouton//
   if (currentIndex > 0) {
-    currentIndex--; // -- pour l'ordre antichronologique//
+    currentIndex--; // si je suis sur la première slide, je recule en arrière 0 --> 3 dans mon tableau//
   } else {
-    currentIndex = slides.length - 1; //slide lenght pour calculer nombre d'element dans l'index// 
+    currentIndex = slides.length - 1; //sinon on revient sur la denrière slide (boucle)//
   }
-   currentdot[currentIndex].classList.add('dot_selected');
-  updateSlide(currentIndex);
-  console.log("Flèche gauche cliquée");
-});
+  currentdot[currentIndex].classList.add('dot_selected'); //dotselected permet de remplir le bullet point de la slide// 
+  updateSlide(currentIndex); //mettre à jour la slide --> fonction qui met à jour texte, titre, alt//
+  console.log("Flèche gauche cliquée"); // pour vérifier sur console//
+}
 
-document.querySelector('.arrow_right').addEventListener('click', () => {
+const arrow_left = document.querySelector('.arrow_left');
+arrow_left.addEventListener('click', arrowleftClick);
+
+
+
+function arrowrightClick() {
   const currentdot = document.querySelectorAll('.dots .dot');
-  currentdot[currentIndex].classList.remove('dot_selected');
-  currentIndex++; //ici ++ pour avancer dans le tableau index en ordre chronologique// 
-  if (currentIndex > slides.length - 1) {
-     currentIndex = 0;    
-  } 
-  currentdot[currentIndex].classList.add('dot_selected');  
-  updateSlide(currentIndex);
+  currentdot[currentIndex].classList.remove('dot_selected'); //j'apelle tous les bullet point et leur supprime le dot selectif//
+  currentIndex++; //pour avancer//
+  if (currentIndex > slides.length - 1) { 
+    currentIndex = 0;//revenir à la première slide si j'arrive à la fin de slide = boucle//
+  }
+  currentdot[currentIndex].classList.add('dot_selected'); //je remplis le dot sur bullet actif//
+  updateSlide(currentIndex); //mis à jour elements de la slide// 
   console.log("Flèche droite cliquée");
-});
+}
 
+const arrow_right = document.querySelector('.arrow_right');
+arrow_right.addEventListener('click', arrowrightClick); //creation de const pour réutilisé la constante plus tard facilement//
 
 
 
@@ -64,13 +72,13 @@ document.querySelector('.arrow_right').addEventListener('click', () => {
 
 let currentIndex = 0;
 
-const bannerImg = document.querySelector(".banner-img");
-const bannerText = document.querySelector("#banner p"); 
+const bannerImg = document.querySelector(".banner-img"); 
+const bannerText = document.querySelector("#banner p"); // je donne à la slide acutelle une image ainsi qu'un//
 
 
 function updateSlide(index) { //elements qui vont changer au changement de slide//
   const slide = slides[index];
-  bannerImg.src = `./assets/images/slideshow/${slide.image}`; //chemin de la nouvelle image"
+  bannerImg.src = `./assets/images/slideshow/${slide.image}`; //chemin de la nouvelle image//
   bannerImg.alt = `Image slide ${index + 1}`; //texte associé à nouvelle image//
   bannerText.innerHTML = slide.tagLine; //tagline de la slide//
 }
